c983a9cb9e452f3121fdec5dd2795bab6c6486a4
Bre Thunholm <bthunholm@gmail.com>
2021-05-04 06:50:44 -1000

    rendered image list search to app component

--- pics-app/pics/src/components/imageList.js
+++ pics-app/pics/src/components/imageList.js
@@ -1,10 +1,11 @@
+/* eslint-disable jsx-a11y/img-redundant-alt */
 /* eslint-disable react/prop-types */
 /* eslint-disable react/destructuring-assignment */
 import React from 'react';
 
 const ImageList = (props) => {
   const images = props.images.map((image) => (
-    <img src={image.urls.regular} alt="pic" />
+    <img src={image.urls.regular} alt="image.alt_description" />
   ));
   return <div>{images}</div>;
 };
