f57e77c7ad14cc9ee18c8a606c61d593471180dd
Bre Thunholm <bthunholm@gmail.com>
2021-05-06 09:15:41 -1000

    added video item css file

--- videos/videos/src/components/VideoItem.js
+++ videos/videos/src/components/VideoItem.js
@@ -1,11 +1,20 @@
 /* eslint-disable react/prop-types */
+import './VideoItem.css';
 import React from 'react';
 
 export default function VideoItem({ video }) {
   return (
-    <div className="item">
-      <img src={video.snippet.thumbnails.medium.url} alt="" key={video.id} />
-      {video.snippet.title}
+    <div className="video-item item">
+      <img
+        className="ui image"
+        src={video.snippet.thumbnails.medium.url}
+        alt=""
+        key={video.id}
+      />
+      <div className="content">
+        <div className="header">{video.snippet.title}</div>
+        <div className="description">A video about something</div>
+      </div>
     </div>
   );
 }
