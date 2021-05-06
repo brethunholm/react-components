be3d44d29da1eb7100964ee88f6bc290f61c23dc
Bre Thunholm <bthunholm@gmail.com>
2021-05-05 18:27:47 -1000

    installed axios added youtube.js added youtube api key

--- videos/videos/src/components/SearchBar.js
+++ videos/videos/src/components/SearchBar.js
@@ -1,7 +1,35 @@
+/* eslint-disable react/destructuring-assignment */
+/* eslint-disable react/no-unused-state */
+/* eslint-disable react/state-in-constructor */
+/* eslint-disable jsx-a11y/label-has-associated-control */
 import React from 'react';
 
 export default class SearchBar extends React.Component {
+  state = { term: '' };
+
+  onInputChange = (e) => {
+    this.setState({ term: e.target.value });
+  };
+
+  onFormSubmit = (e) => {
+    e.preventDefault();
+    // TODO make sure we call, callback from parent component
+  };
+
   render() {
-    return <div>SearchBar</div>;
+    return (
+      <div className="search-bar ui segment">
+        <form onSubmit={this.onFormSubmit} className="ui form">
+          <div className="field">
+            <label>Video Search</label>
+            <input
+              type="text"
+              value={this.state.term}
+              onChange={this.onInputChange}
+            />
+          </div>
+        </form>
+      </div>
+    );
   }
 }
