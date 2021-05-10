229bb0566dd4a76b43b7927085af1c44c796d993
Bre Thunholm <bthunholm@gmail.com>
2021-05-10 08:03:18 -1000

    initialized widget app

---
+++ widgets/widgets/src/App.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import Accordion from './components/Accordion';
+
+const items = [
+  {
+    title: 'What is React?',
+    content: 'React is a front end javascript framework',
+  },
+  {
+    title: 'Why use React?',
+    content: 'React is a favorite JS library among engineers',
+  },
+  {
+    title: 'How do you use React?',
+    content: 'You use React by creating components',
+  },
+];
+
+export default function App() {
+  return (
+    <div>
+      <h1>APP</h1>
+      <Accordion items={items} />
+    </div>
+  );
+}
