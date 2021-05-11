372fbca85d8797060d766a84d40b7fd834732fc5
Bre Thunholm <bthunholm@gmail.com>
2021-05-11 08:34:50 -1000

    implmented wikipedia api call

--- widgets/widgets/src/components/Search.js
+++ widgets/widgets/src/components/Search.js
@@ -1,16 +1,44 @@
+/* eslint-disable react/no-danger */
 /* eslint-disable jsx-a11y/label-has-associated-control */
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
 export default function Search() {
-  const [term, setTerm] = useState('');
+  const [term, setTerm] = useState('kittens');
+  const [results, setResults] = useState([]);
 
   useEffect(() => {
     const search = async () => {
-      await axios.get('');
+      const { data } = await axios.get('https://en.wikipedia.org/w/api.php', {
+        params: {
+          action: 'query',
+          list: 'search',
+          origin: '*',
+          format: 'json',
+          srsearch: term,
+        },
+      });
+      setResults(data.query.search);
     };
     search();
   }, [term]);
+
+  const renderedResults = results.map((result) => (
+    <div key={result.pageid} className="item">
+      <div className="right loaded content">
+        <a
+          className="ui button"
+          href={`https://en.wikipedia.org?curid=${result.pageid}`}
+        >
+          Go
+        </a>
+      </div>
+      <div className="content">
+        <div className="header">{result.title}</div>
+        <span dangerouslySetInnerHTML={{ __html: result.snippet }} />
+      </div>
+    </div>
+  ));
   return (
     <div>
       <div className="ui form">
@@ -24,6 +52,7 @@ export default function Search() {
           />
         </div>
       </div>
+      <div className="ui celled list">{renderedResults}</div>
     </div>
   );
 }
