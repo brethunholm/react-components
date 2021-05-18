509ad965612b859332bad516a66022191a0a7dd9
Bre Thunholm <bthunholm@gmail.com>
2021-05-18 10:40:37 -1000

    added lanugages

--- widgets/src/components/Translate.js
+++ widgets/src/components/Translate.js
@@ -21,7 +21,12 @@ export default function Translate() {
     const [text, setText] = useState('');
     return (
         <div>
+            <div className="ui form ">
+                <div className="field">
+                    <label>Enter Text</label>
             <input value={text} onChange={(e) => setText(e.target.value)} />
+            </div>
+            </div>
             <Dropdown 
             label="Select a Language"
             selected={language} 
