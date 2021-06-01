15d8f9367bbee171fcf3fee3b7bcd2b12cddc17a
Bre Thunholm <bthunholm@gmail.com>
2021-06-01 09:25:38 -0600

    added convert component to translate.js

--- widgets/src/components/Translate.js
+++ widgets/src/components/Translate.js
@@ -1,6 +1,7 @@
 // AIzaSyCHUCmpR7cT_yDFHC98CZJy2LTms-IwDlM
 import React, { useState } from 'react';
 import Dropdown from './Dropdown';
+import Convert from './Convert';
 
 const options = [
     {
@@ -34,6 +35,9 @@ export default function Translate() {
             onSelectedChange={setLanguage} 
             options={options} 
             />
+            <hr />
+            <h3 className="ui header">Output</h3>
+            <Convert text={text} language={language} />
         </div>
     );
 }
