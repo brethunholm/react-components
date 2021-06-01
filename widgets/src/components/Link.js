18d6f4907bcfd0dd66d94a700d3934f417d3bb29
Bre Thunholm <bthunholm@gmail.com>
2021-06-01 14:59:52 -0600

    created link component to be utilized by nav header component

---
+++ widgets/src/components/Link.js
@@ -0,0 +1,10 @@
+
+import React from 'react'
+
+export default function Link({className, href, children}) {
+    return (
+        <a className={className} href={href}>
+           {children}
+        </a>
+    )
+}
