0bb791478e8a2676b0806a5625cbd09cd86efd70
Bre Thunholm <bthunholm@gmail.com>
2021-06-02 09:44:47 -0600

    refactored search bar component to make use of hooks

--- hooks-video-refactor/src/components/SearchBar.js
+++ hooks-video-refactor/src/components/SearchBar.js
@@ -1,37 +1,29 @@
-/* eslint-disable react/prop-types */
-/* eslint-disable react/destructuring-assignment */
-/* eslint-disable react/no-unused-state */
-/* eslint-disable react/state-in-constructor */
 /* eslint-disable jsx-a11y/label-has-associated-control */
-import React from 'react';
+/* eslint-disable react/prop-types */
 
-export default class SearchBar extends React.Component {
-  state = { term: '' };
+import React, { useState } from 'react';
 
-  onInputChange = (e) => {
-    this.setState({ term: e.target.value });
+export default function SearchBar({ onFormSubmit }) {
+  const [term, setTerm] = useState('');
+
+  const onInputChange = (e) => {
+    setTerm(e.target.value);
   };
 
-  onFormSubmit = (e) => {
+  const onSubmit = (e) => {
     e.preventDefault();
 
-    this.props.onFormSubmit(this.state.term);
+    onFormSubmit(term);
   };
 
-  render() {
-    return (
-      <div className="search-bar ui segment">
-        <form onSubmit={this.onFormSubmit} className="ui form">
-          <div className="field">
-            <label>Video Search</label>
-            <input
-              type="text"
-              value={this.state.term}
-              onChange={this.onInputChange}
-            />
-          </div>
-        </form>
-      </div>
-    );
-  }
+  return (
+    <div className="search-bar ui segment">
+      <form onSubmit={onSubmit} className="ui form">
+        <div className="field">
+          <label>Video Search</label>
+          <input type="text" value={term} onChange={onInputChange} />
+        </div>
+      </form>
+    </div>
+  );
 }
